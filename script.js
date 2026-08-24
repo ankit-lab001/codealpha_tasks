b539cb86993007b3294ea8b7dd94be0e8f9b0550
ankit-lab001 <ankit9905810817@gmail.com>
2026-08-24 21:01:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+
+    if (display.value === "Error") {
+        display.value = "";
+    }
+
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        if (display.value === "") return;
+
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+
+    const key = event.key;
+
+    if (key >= "0" && key <= "9") {
+        appendValue(key);
+    }
+
+    else if (key === "+" || key === "-" || key === "*" || key === "/") {
+        appendValue(key);
+    }
+
+    else if (key === ".") {
+        appendValue(".");
+    }
+
+    else if (key === "Enter" || key === "=") {
+        event.preventDefault();
+        calculate();
+    }
+
+    else if (key === "Backspace") {
+        deleteLast();
+    }
+
+    else if (key === "Escape") {
+        clearDisplay();
+    }
+
+});
